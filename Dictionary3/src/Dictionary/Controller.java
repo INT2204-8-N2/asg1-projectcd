e5074f91f4e023a60b4219fb52e59026bb7b723f
duong17020661 <43227742+duong17020661@users.noreply.github.com>
2018-10-08 21:20:37 +0700

    Update Controller.java
    sss

--- Dictionary3/src/Dictionary/Controller.java
+++ Dictionary3/src/Dictionary/Controller.java
@@ -1,6 +1,7 @@
 package Dictionary;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
@@ -15,16 +16,23 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 
 public class Controller implements Initializable {
     @FXML
     private TableView<Dictionary> tableView;
     @FXML
-    private TableColumn<Dictionary, String> english;
+    private TableColumn<Dictionary,String> english;
     @FXML
     private TableColumn<Dictionary, String> vietnamese;
     @FXML
@@ -37,18 +45,36 @@ public class Controller implements Initializable {
     private TextArea textArea;
 
     private Dictionary dictionary;
-    private ObservableList<Dictionary> data = FXCollections.observableArrayList();
+    private List<Dictionary> list = new ArrayList<Dictionary>();
+    private ObservableList<Dictionary> data = FXCollections.observableArrayList(list);
+
     int row = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        data.add(new Dictionary("Hello", "Xinchao"));
-        data.add(new Dictionary("Cello", "Xinchao"));
-        data.add(new Dictionary("Fello", "Xinchao"));
-        data.add(new Dictionary("Hello", "Xinchao"));
-
+        String fileName = "C:\\Users\\ADMIN\\Downloads\\anhviet109K.txt";//bạn hãy thay đổi đường dẫn tới file của bạn
+        String content = null;//đưa về chuẩn utf-8
+        try {
+            content = new String(Files.readAllBytes(Paths.get(fileName)),
+                    StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] word = content.split("\n\n");
+        ArrayList<String> anh = new ArrayList<String>();
+        for(String w:word){
+            String[] b;
+            b=w.split("/");
+            anh.add(b[0]);
+        }
+        for (int i=0;i<word.length;i++){
+            Dictionary a = new Dictionary();
+            a.setVietnamese(word[i]) ;
+            a.setEnglish(anh.get(i));
+            data.add(a);
+        }
         english.setCellValueFactory(new PropertyValueFactory<>("english"));
-        vietnamese.setCellValueFactory(new PropertyValueFactory<>("vietnamese"));
+       // vietnamese.setCellValueFactory(new PropertyValueFactory<>("vietnamese"));
         tableView.setItems(data);
     }
     public void Search(){
@@ -62,9 +88,9 @@ public class Controller implements Initializable {
                 if (dictionary.getEnglish().toLowerCase().contains(lowerCaseFilter)) {
                     return true;
                 }
-                else if (dictionary.getVietnamese().toLowerCase().contains(lowerCaseFilter)) {
-                    return true;
-                }
+//                else if (dictionary.getVietnamese().toLowerCase().contains(lowerCaseFilter)) {
+//                    return true;
+//                }
                 return false;
             });
         });
@@ -129,12 +155,12 @@ public class Controller implements Initializable {
         }
     }
     @FXML
-    public void tableClick(MouseEvent e) {
+    public void tableClick(MouseEvent e) throws IOException {
         if (MouseButton.PRIMARY == e.getButton() && e.getClickCount() == 2) {
             dictionary = tableView.getSelectionModel().getSelectedItem();
             anh.setText(dictionary.getEnglish());
-            viet.setText(dictionary.getVietnamese());
-            textArea.setText(dictionary.getEnglish());
+        //    viet.setText(dictionary.getVietnamese());
+            textArea.setText(dictionary.getVietnamese());
             row = tableView.getSelectionModel().getSelectedIndex();
             Disable(true);
         }
