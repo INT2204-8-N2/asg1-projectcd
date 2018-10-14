06eaea8db05c64a2bdeaa47a457b84b4981a8fc1
vietcuongk99 <43227743+vietcuongk99@users.noreply.github.com>
2018-10-14 23:49:35 +0700

    up

--- src/Dictionary/Controller.java
+++ src/Dictionary/Controller.java
@@ -210,6 +210,37 @@ public class Controller implements Initializable {
         Dictionary selected = tableView.getSelectionModel().getSelectedItem();
         data.remove(selected);
     }
+    public void Speech(ActionEvent event) {
+            Dictionary selected = tableView.getSelectionModel().getSelectedItem();
+            if (selected != null)
+            try {
+                // set property as Kevin Dictionary
+                System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
 
+                        // Register Engine
+                Central.registerEngineCentral("com.sun.speech.freetts.jsapi.FreeTTSEngineCentral");
+
+                        // Create a Synthesizer
+                Synthesizer synthesizer =
+                        Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
+
+                        // Allocate synthesizer
+                synthesizer.allocate();
+
+                        // Resume Synthesizer
+                synthesizer.resume();
+
+                        // speaks the given text until queue is empty.
+                synthesizer.speakPlainText(selected.getEnglish(), null);
+                synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
+
+                        // Deallocate the Synthesizer.
+                synthesizer.deallocate();
+            } catch (Exception ex) {
+                Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
 }
+
+
 
